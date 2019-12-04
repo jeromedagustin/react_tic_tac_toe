3d17d7d4357eadb33f3d9647645248955ef6790a
theingredients <theingredientscollective@gmail.com>
2019-12-04 14:58:58 -0800

    Updates

---
+++ precache-manifest.1923a831e31db28cb1363bd88fe394e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b66826f44b3bde02c2a695b519bedd",
+    "url": "/react_tic_tac_toe/index.html"
+  },
+  {
+    "revision": "de1cc24e4a62da4132fb",
+    "url": "/react_tic_tac_toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "f1263d1de28c1e77657a",
+    "url": "/react_tic_tac_toe/static/js/2.790b459f.chunk.js"
+  },
+  {
+    "revision": "de1cc24e4a62da4132fb",
+    "url": "/react_tic_tac_toe/static/js/main.027fee56.chunk.js"
+  },
+  {
+    "revision": "01ac10ff0a2942d6ffcb",
+    "url": "/react_tic_tac_toe/static/js/runtime-main.fa6b2c65.js"
+  }
+]);
